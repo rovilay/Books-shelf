8ed80138d0437b05d14748a96198b26b324f5a2e
rovilay <rovilayjnr01@gmail.com>
2018-12-27 12:56:24 +0100

    feat(Auth pages): create Login and signup page
    - Create login page and form
    - Create Signup page and form
    - Validate forms
    
    [Delivers]

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HomePage from '../components/Home/Home.vue'
-import Dashboard from '../components/Dashboard/Dashboard.vue'
+import HomePage from '../components/Home/Home.vue';
+import Dashboard from '../components/Dashboard/Dashboard.vue';
+import AuthPage from '../components/AuthPage/AuthPage.vue';
 
 Vue.use(Router);
 
@@ -16,7 +17,17 @@ export default new Router({
             path: '/dashboard',
             name: 'Dashboard',
             component: Dashboard
-        }
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: AuthPage
+        },
+        {
+            path: '/signup',
+            name: 'Signup',
+            component: AuthPage
+        },
     ]
 })
  
